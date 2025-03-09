68f109fc1f04f2eb6106726f204e636c7296bd30
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-09 12:18:52 +0545

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 apt update -y
-apt install -y gcc make libx11-dev libxft-dev libxext-dev libharfbuzz-dev
+apt install -y gcc make libx11-dev libxft-dev libxext-dev libharfbuzz-dev zip
 git clone --depth=1 https://github.com/instantOS/st-instantos
 make
 make PREFIX="./stpkg" install
