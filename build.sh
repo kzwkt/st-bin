7b1bb5e0ec106355b4a3dcf220ae07b5520081e9
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-23 11:06:24 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #
 apt update -y
-apt install -y libx11-dev libxft-dev libxext-dev libharfbuzz-dev zip git gcc make ca-certificates --no-install-recommends
+apt install -y libx11-dev libxft-dev libxext-dev libharfbuzz-dev zip git gcc make ca-certificates binutils --no-install-recommends
 git clone --depth=1 https://github.com/kzwkt/st-instantos
 cd st-instantos
 make
+strip st
